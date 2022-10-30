14b1735fed78a804901d82ae2bb4063d00e846d9
Eemil <eemil.arola@gmail.com>
2022-10-30 17:19:29 +0200

    100% varastohaarautuvuuskattavuushomma

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -5,6 +5,8 @@ from varasto import Varasto
 class TestVarasto(unittest.TestCase):
     def setUp(self):
         self.varasto = Varasto(10)
+        self.varasto2 = Varasto(-10, -2)
+        self.varasto3 = Varasto(2,4)
 
     def test_konstruktori_luo_tyhjan_varaston(self):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
@@ -38,3 +40,40 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+
+    def test_laitetaan_liikaa(self):
+        self.varasto.lisaa_varastoon(12)
+        #varastossa ei pitäisi olla tilaa
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+    
+    def test_otetaan_liikaa(self):
+        self.varasto.ota_varastosta(12)
+        #varastossa ei pitäisi olla tilaa
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+
+    def test_printtaustoimii(self):
+        self.varasto.ota_varastosta(10)
+        testistr = str(self.varasto)
+        
+        #saldon pitäisi olla 0, tilavuuden 10
+        self.assertEqual(testistr, "saldo = 0.0, vielä tilaa 10.0")
+
+    def test_ota_negatiivista(self):
+        self.varasto3.ota_varastosta(-2)
+
+        #ei pitäisi vaikuttaa
+        self.assertAlmostEqual(self.varasto3.paljonko_mahtuu(), 0)
+
+    def test_ei_voi_luoda_negsaldo(self):
+        self.assertAlmostEqual(self.varasto2.saldo, 0)
+
+    def test_ei_voi_luoda_negvol(self):
+        self.assertAlmostEqual(self.varasto2.tilavuus, 0)
+
+    def test_ei_voi_luoda_liikaasaldoa(self):
+        self.assertAlmostEqual(self.varasto3.saldo, 2)
+
+    def test_ei_lisata_neg(self):
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
